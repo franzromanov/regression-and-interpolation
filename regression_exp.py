13e87c22200b3ca0756dae3e2202a5314651b1f5
franzromanov <114895290+franzromanov@users.noreply.github.com>
2022-11-06 00:41:31 +0800

    Add files via upload

---
+++ regression_exp.py
@@ -0,0 +1,180 @@
+#refference_section
+from tabulate import tabulate
+import math
+#import matplotlib.pyplot as plt
+#from sympy import *
+
+print("""
+>>>>>>REGULAR EXPONENTIAL REGRESSION<<<<<<
+*
+*
+*
+*
+*
+*
+*
+*
+*
+***********WELCOME***********
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>""")
+
+
+
+
+
+#inp_section
+Xs=input("data(x): ")
+Ys=input("data(y): ")
+
+#casting_mechanism
+
+#array_str
+Xs=Xs.split(",")
+Ys=Ys.split(",")
+
+Xint=[]
+Yint=[]
+
+#array_float
+for x in Xs:
+	Xint.append(float(x))
+for y in Ys:
+	Yint.append(float(y))
+
+
+#calculation_section
+#y=a*x**b
+
+#change_var to P and Q
+P=[]
+Q=[]
+dumper=0
+
+for x in Xint:
+	dumper=math.log(x,10)
+	Q.append(dumper)
+
+dumper=0
+
+for y in Yint:
+	dumper=math.log(y,10)
+	P.append(dumper)
+
+
+#find_b
+pqup=[]
+pqupdump=0
+pup2=0
+qup2=0
+qlow1=0
+qlow1dump=[]
+qlow2=0
+
+#xup2,yup2
+for p in P:
+	pup2=pup2+p
+
+for q in Q:
+	qup2=qup2+q
+#print(pup2*qup2)
+#xyup
+for p,q in zip(P,Q):
+	pqup.append(p*q)
+print
+for pq in pqup:
+	pqupdump=pqupdump+pq
+#print(pqupdump)
+pqupdump=pqupdump*len(P)
+#print(pqupdump)
+
+#xlow1
+for q in Q:
+	qlow1=qlow1+(q**2)
+	qlow1dump.append(q**2)
+
+qlow1=len(Q)*qlow1
+#print(qlow1)
+
+#xlow2
+for q in Q:
+	qlow2=qlow2+q
+qlow2=(qlow2)**2
+#print(qlow2)
+#b_calc:
+b=(pqupdump-(pup2*qup2))/(qlow1-qlow2)
+#print(b)
+
+#table section
+table=[]
+tabledump=[]
+count=0
+
+for p,q,pq,pexp in zip (P,Q,pqup,qlow1dump):
+	count=count+1
+	table.append(count)
+	table.append(p)
+	table.append(q)
+	table.append(pq)
+	table.append(pexp)
+	tabledump.append(table)
+	table=[]
+#print(tabledump)
+
+header=["count","p","q","pq","q**2"]
+print(tabulate(tabledump, headers=header, tablefmt="grid"),"\n")
+
+#find a
+
+#pmean
+pmean=0
+qmean=0
+xmean=0
+ymean=0
+for p in P:
+	pmean=pmean+p
+pmean=pmean/len(P)
+#print(xmean)
+
+#qmean
+for q in Q:
+	qmean=qmean+q
+qmean=qmean/len(Q)
+
+#ymean
+for y in Yint:
+	ymean=ymean+y
+ymean=ymean/len(Yint)
+#print(ymean)
+
+A=pmean-(b*qmean)
+a=10**A
+print("\n\n\n")
+
+#find r
+Dt=0
+D=0
+
+for x,y in zip(Xint,Yint):
+	D=D+(y-(a*(x**b)))**2
+for y in Yint:
+	Dt=Dt+((y-ymean)**2)
+
+
+r=((Dt-D)/Dt)
+
+
+#equation_section
+print(f"""######EQUATION SECTION######
+*
+*
+*
+*
+the equation is ==> [y={a}x^{b}] 
+The r Value is >>> {r}
+
+<<<Job_Done>>>
+""")
+
+
+
+
